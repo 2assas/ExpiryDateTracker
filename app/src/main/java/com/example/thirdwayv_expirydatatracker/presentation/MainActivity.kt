33e5991b6c575fdfc61c13701c2062d798284d49
Mahmoud Adel <el2assas@gmail.com>
2022-01-23 04:24:41 +0200

    Project Structure

--- app/src/main/java/com/example/thirdwayv_expirydatatracker/presentation/MainActivity.kt
+++ app/src/main/java/com/example/thirdwayv_expirydatatracker/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.thirdwayv_expirydatatracker
+package com.example.thirdwayv_expirydatatracker.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.thirdwayv_expirydatatracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
